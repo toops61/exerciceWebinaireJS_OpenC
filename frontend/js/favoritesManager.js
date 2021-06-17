fd29c6ea6f35968917a75e80d62a43f6358d8b4e
Toops61 <toops61@gmail.com>
2021-06-17 10:35:23 +0200

    mise en place JS

---
+++ frontend/js/favoritesManager.js
@@ -0,0 +1,22 @@
+/**
+ * Gestion des favoris, enregistrement d'un article en favoris, retrait d'un article des favoris et récupération de la liste des favoris
+ */
+
+function addFavorites(articlesID){
+    let listFavorites = getFavorites();
+    listFavorites.push(articlesID);
+    saveFavorites(listFavorites);
+}
+
+function getFavorites(){
+    let listFavorites = localStorage.getItem("listFavorites");
+    if(listFavorites == null){
+        return [];
+    }else{
+        return JSON.parse(listFavorites);
+    }
+}
+
+function saveFavorites(listFavorites){
+    localStorage.setItem("listFavorites",JSON.stringify(listFavorites));
+}
